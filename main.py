73ee50bda0e7929cd0f67690f6f3ff08ac26fee2
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2026-02-07 18:01:46 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import pgzrun
 WIDTH=600
 HEIGHT=600
 TITLE="PAQUETA CRISPS"
+gravity=1000
 
 class Ball():
     def __init__(self):
         self.type="football"
         self.color="red"
-        self.radius=100
+        self.radius=50
         self.x=300
         self.y=300
+        self.vx=80
+        self.vy=0
+        
     
         
     def draw(self):
         screen.draw.filled_circle((self.x,self.y),self.radius, self.color)
-     
+
 
 object=Ball()
 def draw():
@@ -27,8 +31,25 @@ def draw():
 
 
 
-def update():
-    pass
+def update(dt): 
+    uy=object.vy
+    object.vy+=gravity * dt 
+    object.y+=(uy + object.vy) * 0.5
+    if object.y>=HEIGHT-object.radius:
+        object.vy=object.vy * -0.9
+        object.y=HEIGHT-object.radius
+    object.x+=object.vx * dt
+    if object.x>=WIDTH-object.radius:
+        object.vx=object.vx* -1
+        object.x=WIDTH-object.radius
+    if object.x<object.radius:
+        object.x=object.radius
+        object.vx=object.vx *-1
+
+
+    
+
+
 
 
 
